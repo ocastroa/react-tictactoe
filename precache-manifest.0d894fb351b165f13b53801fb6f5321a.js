f08a81489f39a83bb5e60733d167a16f9689ee46
Oscar Castro <oscar.castro@pubnubs-MacBook-Air.local>
2019-07-31 12:16:37 -0700

    Updates

---
+++ precache-manifest.0d894fb351b165f13b53801fb6f5321a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7f8389d3809d4bed923399cdd5b88e",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "5a2d478f56620dbc5714",
+    "url": "/react-tictactoe/static/css/main.1f355159.chunk.css"
+  },
+  {
+    "revision": "a0ba22936bb3e99c591a",
+    "url": "/react-tictactoe/static/js/2.b770d6c5.chunk.js"
+  },
+  {
+    "revision": "5a2d478f56620dbc5714",
+    "url": "/react-tictactoe/static/js/main.0bde601f.chunk.js"
+  },
+  {
+    "revision": "32b32cc97a4c7174c6c0",
+    "url": "/react-tictactoe/static/js/runtime~main.f8e643ec.js"
+  }
+]);
